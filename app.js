3435525ac1513c3427f0b702353f18b744fac3a5
Roy <royp@Roy.local>
2019-02-11 07:33:35 +0100

    First Routes

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 const express = require('express');
+const pug = require('pug');
 const path = require('path');
-const body-parser = require('body-parser')
-const cookie-parser = require('cookie-parser')
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const {projects} = require('./data.json');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
+app.get("/", function(req, res, next){
+  res.render('index', {projects});
+})
+
+app.get("/about", function(req, res, next){
+  res.render('about');
+})
+
+app.get("/project/:id", function(req, res, next){
+  const project = projects[req.params.id];
+    // findUserByUsername(username, function(error, user) {
+  //   if (error) return next(error);
+  //   return response.render('user', user);
+  // });
+  res.render('project',{project});
+})
+
+
+app.listen(3000)
